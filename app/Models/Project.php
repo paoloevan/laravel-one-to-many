cb8924518708c286782409f84e9b26fbe6d67c97
paoloevan <paoloev1900@gmail.com>
2023-01-16 15:49:02 +0100

    add relationship

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
 class Project extends Model
 {
-    protected $fillable = ['title', 'cover_image', 'team', 'duration', 'description', 'slug_title'];
+    protected $fillable = ['title', 'cover_image', 'team', 'duration', 'description', 'slug_title', 'type_id'];
 
     use HasFactory;
 
@@ -17,5 +18,10 @@ class Project extends Model
         $slug_title = Str::slug($title);
 
         return $slug_title;
+    }
+
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(Type::class);
     }
 }
